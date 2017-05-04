15087b28cde75d7a7f401e7ef07c66350ac8dc2d
jwitchel <jwitchel@colevalleygroup.com>
2017-05-03 20:53:16 -0600

    Added datatables.net package and configured Review Ad Requests with a basic table.

--- client/reviewAdRequests.js
+++ client/reviewAdRequests.js
@@ -2,8 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './reviewAdRequests.html';
-   //var foo = Ads.find().fetch();
- //console.log(foo)
+
 Template.reviewAdRequests.events({
     'click .foo': function(){
          var requests = [];
@@ -16,18 +15,36 @@ Template.reviewAdRequests.events({
     },
 });
 
+
+dataTableData = function () {
+    var x=  Ads.find({createdBy : Meteor.userId() }).fetch(); // or .map()
+    return x;
+};
+
+var optionsObject = {
+    columns: [{
+        title: 'ID',
+        data: '_id', // note: access nested data like this
+        className: 'nameColumn'
+    },
+    {title: 'All Schools',
+    data: 'allSchools',
+    className: 'nameColumn'
+    },
+    {title: 'Regular',
+    data: 'regular',
+    className: 'nameColumn'
+    }],
+    // ... see jquery.dataTables docs for more
+}
+
+
 Template.DisplayAdsTemplate.helpers({
     displayAds: function(){
-        var requests = [];
-        var foo = Ads.find().fetch(); 
-         for(var i =0; i < foo.length; i++){
-            if(foo[i].createdBy === Meteor.userId()){
-                requests.push(foo[i])
-            }
-         }
-         return requests;  
+        return Ads.find({createdBy : Meteor.userId() }).fetch();         
     },
-    'otherHelperFunction': function(){
-        return "Some other function";
-    }
+    reactiveDataFunction: function () {
+        return dataTableData;
+    },
+    optionsObject: optionsObject // see below
 });
