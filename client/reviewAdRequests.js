c0febb236bf7bb0e35b82c85b2d4a6c4481aac41
John Witchel <jwitchel@colevalleygroup.com>
2017-05-03 19:46:26 -0600

    Helpers were incorrectly attached to the reviewAdRequests template.  They needed to be attached to the foo template which I renamed to DisplayAdsTemplate for clarity.  Everything else ran perfectly once the helper was attached to the correct template

--- client/reviewAdRequests.js
+++ client/reviewAdRequests.js
@@ -15,22 +15,17 @@ Template.reviewAdRequests.events({
          } 
     },
 });
-// Template.reviewAdRequests.helpers({
-//     displayAds: function(){
-//         var requests = [];
-//         var foo = Ads.find().fetch(); 
-//          for(var i =0; i < foo.length; i++){
-//             if(foo[i].createdBy === Meteor.userId()){
-//                 requests.push(foo[i])
-//             }
-//          }
-//          return requests;  
-//     },
-// });
 
-Template.reviewAdRequests.helpers({
-    'displayAds': function(){
-        console.log(foo);
+Template.DisplayAdsTemplate.helpers({
+    displayAds: function(){
+        var requests = [];
+        var foo = Ads.find().fetch(); 
+         for(var i =0; i < foo.length; i++){
+            if(foo[i].createdBy === Meteor.userId()){
+                requests.push(foo[i])
+            }
+         }
+         return requests;  
     },
     'otherHelperFunction': function(){
         return "Some other function";
