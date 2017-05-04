e96178bf43909f46a99f973ec1a1fcaadddcc27e
jwitchel <jwitchel@colevalleygroup.com>
2017-05-03 23:36:12 -0600

    Redirect user to from createAd to uploadImage after record creation.

--- client/createAdd.js
+++ client/createAdd.js
@@ -34,12 +34,9 @@ Template.CreateAd.events({
             createdBy: currUser,
             creationDate: new Date() 
         });
-        // console.log(allGrades)
-        // console.log(highSchool)
-        // console.log(middleSchool)
-        // console.log(lowerSchool)      
-        // console.log(allowAtypical.value)
-        // console.log(lowerLimit)
-        // console.log(upperLimit)
+        
+        // Send the user to the next step
+        Router.go('/uploadImage');
+
     }
 });
