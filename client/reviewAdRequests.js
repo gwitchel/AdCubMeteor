cacf7eb321a19468ac9530e66424245228b366ea
jwitchel <jwitchel@colevalleygroup.com>
2017-05-06 22:07:00 -0600

    Finally got the schools working in DataTables using a virtual Mongo collection.  new Mongo.Collection(null) <-- Pass in the null

--- client/reviewAdRequests.js
+++ client/reviewAdRequests.js
@@ -8,6 +8,17 @@ dataTableData = function () {
     return x;
 };
 
+schoolTableData = function(){
+        //Create a client side only Mongo collection
+        var LocalSchools = new Mongo.Collection(null);
+        for (var i = 0; i < schools.length; i++) {
+            LocalSchools.insert(schools[i]);
+        }
+        var x = LocalSchools.find().fetch();
+        return x;
+}
+
+
 var optionsObject = {
     "columnDefs": [ {
     "targets" : 2 ,
@@ -31,6 +42,20 @@ var optionsObject = {
     // ... see jquery.dataTables docs for more at https://datatables.net/
 }
 
+var schoolObject ={
+    columns: [
+        {
+            title: 'School Name',
+            data: 'schoolName', // note: access nested data like this
+            className: 'nameColumn'
+        },
+        {
+            title: 'School County',
+            data: 'schoolCounty',
+            className: 'nameColumn'
+        }
+        ],
+}
 
 Template.DisplayAdsTemplate.helpers({
     displayAds: function(){
@@ -39,5 +64,10 @@ Template.DisplayAdsTemplate.helpers({
     reactiveDataFunction: function () {
         return dataTableData;
     },
-    optionsObject: optionsObject // see below
+    optionsObject: optionsObject,
+    schoolOptionObject: schoolObject,
+    schoolDataFunction: function(){
+        return schoolTableData;
+    }
+
 });
